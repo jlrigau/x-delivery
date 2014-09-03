39e10394094e77214f7fb49fa2f45bf5ffcfec75
Thomas Auffredou <tauffredou@xebia.fr>
2014-09-03 16:13:15 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -x
+#!/bin/bash
 
 APP_REPO=$1
 APP_ENV=$2
 
-# Read properties
+function usage(){
+  echo deploy.sh /PATH/TO/PROJECT ENV
+}
+
 function getProp(){
   grep "$1=" $APP_REPO/env/$APP_ENV.properties |cut -d"=" -f2
 }
@@ -12,12 +15,28 @@ function getGlobal(){
   grep "$1=" $APP_REPO/env/docker.properties |cut -d"=" -f2
 }
 
-#PORT_MAPPING=$(grep ports.mapping $APP_REPO/env/$APP_ENV.properties |cut -d"=" -f2)
-PORT_MAPPING=$(getProp ports.mapping)
+# Check parameters
+if [ $# -lt 2 ];then
+  usage
+  exit 1
+fi
+
+if [ ! -d $APP_REPO ];then
+  echo Unable to find project $APP_REPO
+  exit 1
+fi
+
+if [ ! -e $APP_REPO/env/$APP_ENV.properties ];then
+  echo Unable to find $APP_REPO/env/$APP_ENV.properties
+  exit 1
+fi
+
+# Read properties
 DOCKER_REPOSITORY=$(getGlobal docker.repository)
 DOCKER_VERSION=$(getGlobal docker.repository.version)
 CORE_OS=$(getProp coreos.ip)
 
+PORT_MAPPING=$(getProp ports.mapping)
 PORTS_ARG=""
 for MAP in $(echo $PORT_MAPPING | tr "," "\n" );do
   PORTS_ARG="$PORTS_ARG -p $MAP"
@@ -27,6 +46,4 @@ ENV_ARGS="env=$APP_ENV"
 
 # Go
 chmod 600 $APP_REPO/env/$APP_ENV.key.pem
-
-#https://github.com/Jean-Eudes/hello-world
 ssh -oStrictHostKeyChecking=no -i $APP_REPO/env/$APP_ENV.key.pem core@$CORE_OS "docker stop $APP_ENV ;docker rm $APP_ENV; docker run -d -e $ENV_ARGS --name=$APP_ENV $PORTS_ARG $DOCKER_REPOSITORY:$DOCKER_VERSION"
